0ecee6d66a790afca046590db7d092651b9f4302
Corey H <c.l.halicke@gmail.com>
2019-08-12 17:41:37 +0000

    first commit

---
+++ lib/quick/stock/market/facts.rb
@@ -0,0 +1,12 @@
+require "quick/stock/market/facts/version"
+
+module Quick
+  module Stock
+    module Market
+      module Facts
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/quick/stock/market/facts/version.rb
@@ -0,0 +1,9 @@
+module Quick
+  module Stock
+    module Market
+      module Facts
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
